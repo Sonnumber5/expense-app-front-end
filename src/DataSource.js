9dab1bd17caecc4102af89817855aeb3f96d41b3
Sonnumber5 <157999365+Sonnumber5@users.noreply.github.com>
2025-09-23 22:38:35 -0400

    updated datasource for activity 4 assignment

--- src/DataSource.js
+++ src/DataSource.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://expense-app-back-end-0bd2f4b6085c.herokuapp.com'
+    baseURL: 'http://jack-parkison.eba-xf32yiqp.us-east-2.elasticbeanstalk.com'
 });
